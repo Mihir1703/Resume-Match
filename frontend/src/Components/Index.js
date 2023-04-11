7268c9cda0aa5ae50953b68f9bcfa5aac692fd0f
mihir <you@example.com>
2023-04-11 12:40:19 +0000

    main; gi

--- frontend/src/Components/Index.js
+++ frontend/src/Components/Index.js
@@ -5,6 +5,7 @@ const Index = () => {
     return (
         <><nav class="bg-white shadow dark:bg-gray-800">
             <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+                <Link to="/login" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Login</Link>
                 <Link to="/allJobs" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Job Postings</Link>
 
                 <Link to="/post_job" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Create Job Posting</Link>
